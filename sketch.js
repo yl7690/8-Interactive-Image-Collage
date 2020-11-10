2850c6fa1e0e65fd854f9d2215b36d56c630fe8c
yl7690 <yl7690@nyu.edu>
2020-11-09 21:27:33 -0500

    added the second filter- raining - and reduced the sizes of the images

--- sketch.js
+++ sketch.js
@@ -1,3 +1,21 @@
+let sky1;
+let sky2;
+let sky3;
+let sky4;
+let sky5;
+let sky6;
+let sky7;
+let sky8;
+let sky9;
+let sky10;
+let sky11;
+let sky12;
+let sky13;
+let sky14;
+let sky15;
+let sky16;
+let front;
+
 function preload() {
     sky1 = loadImage('images/sky1.jpg');
     sky2 = loadImage('images/sky2.jpg');
@@ -50,6 +68,8 @@ function draw() {
     
     if (ballX < width/2 && ballY < height/2) {
         sunset();
+    } else if (ballX < width/2 && ballY > height/2) {
+        raining();        
     }
     
     frame();
@@ -76,42 +96,68 @@ function sunset() {
         
             if (b > 170 && r > 170 && g > 170) {
             
-              pixels[index+0] = r + 100;
-              pixels[index+1] = g + 70;
-              pixels[index+2] = b - 30;
-              pixels[index+3] = 255;
+                pixels[index+0] = r + 100;
+                pixels[index+1] = g + 70;
+                pixels[index+2] = b - 30;
+                pixels[index+3] = 255;
             } else if (b > 100){
           
-              pixels[index+0] = 80 + r;
-              pixels[index+1] = g;
-              pixels[index+2] = b - 90;
-              pixels[index+3] = 255;
+                pixels[index+0] = 80 + r;
+                pixels[index+1] = g;
+                pixels[index+2] = b - 90;
+                pixels[index+3] = 255;
             }
-          }
         }
-    updatePixels();
-    
+    }
+    updatePixels();  
+}
+
+
+function raining() { 
+    loadPixels();
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            var index = (x + y * width)*4;
+          
+            let index2;
+          
+            for (let i=1; i<random(10);i++) {
+                index2 = (x + (y+i) * width) * 4;
+            }
+        
+            let r = pixels[index+0];
+            let g = pixels[index+1];
+            let b = pixels[index+2];
+            let a = pixels[index+3];   
+         
+            pixels[index2+0] = r;
+            pixels[index2+1] = g;
+            pixels[index2+2] = b;
+            pixels[index2+3] = 255;
+        }
+    }
+    updatePixels(); 
 }
 
 
 function frame() {
     image(front, 0, 0, width, height);
   
-  front.loadPixels();
+    front.loadPixels();
     for (var fy = 0; fy < front.height; fy++) {
-      for (var fx = 0; fx < front.width; fx++) {
-        var index = (fx + fy * front.width)*4;
+        for (var fx = 0; fx < front.width; fx++) {
+            var index = (fx + fy * front.width)*4;
         
-        let fr = front.pixels[index+0];
-        let fg = front.pixels[index+1];
-        let fb = front.pixels[index+2];
-        let fa = front.pixels[index+3];   
-        
-        if (fr > 200 && fg < 200 && fb < 200) {
+            let fr = front.pixels[index+0];
+            let fg = front.pixels[index+1];
+            let fb = front.pixels[index+2];
+            let fa = front.pixels[index+3];   
           
-          front.pixels[index+3] = 0;
+            if (fr > 200 && fg < 200 && fb < 200) {
+          
+                front.pixels[index+3] = 0;
+            }
         }
-      }
     }
-  front.updatePixels();
+    front.updatePixels();
 }
